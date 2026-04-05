8e8929d5b05a1e800860d897600e426b77ce7938
SyaPratama <rasyaputraq17@gmail.com>
2026-04-06 03:14:42 +0700

    init: project initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,37 @@
 import type { NextConfig } from "next";
+import path from "path";
+
+
+const root_dir = path.join(__dirname, '..');
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack:{
+    root: root_dir,
+    resolveAlias: {
+      '@': '*',
+    },
+    resolveExtensions: ['.mdx', '.tsx', '.ts', '.jsx', '.js', '.mjs', '.json'],
+    rules: {
+      '*.svg': [
+        {
+          condition: 'browser',
+          loaders: [
+            {
+              loader: '@svgr/webpack',
+              options:{
+                icon: true,
+              }
+            }
+          ],
+          as: '*.ts',
+        },
+      ]
+    }
+  },
+  typescript:{
+    ignoreBuildErrors: false,
+    tsconfigPath: 'tsconfig.json'
+  }
 };
 
 export default nextConfig;
